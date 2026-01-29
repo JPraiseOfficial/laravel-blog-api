e438797e4ffac01c6f7c922913aaaec0bfb8937c
JPraiseOfficial <jimmypraise.dev@gmail.com>
2026-01-29 10:57:57 +0100

    added MyUsers resources

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\MyUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::apiResource("users", MyUserController::class);
