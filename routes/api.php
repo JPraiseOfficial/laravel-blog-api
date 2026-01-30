e4f617ab723fa96c273ce93dfb395b827cf17775
JPraiseOfficial <jimmypraise.dev@gmail.com>
2026-01-30 14:25:21 +0100

    converted  project to blog api

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Controllers\MyUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-Route::apiResource("users", MyUserController::class);
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
