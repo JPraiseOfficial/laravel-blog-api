027d97cf8a8b9f12507eba9d8882c2d759bf950b
JPraiseOfficial <jimmypraise.dev@gmail.com>
2026-02-03 10:37:24 +0100

    changed route name and restructured it

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,13 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // AUTH ROUTES
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::post('/change-password', [AuthController::class, 'changePassword'])->middleware('auth:sanctum');
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', 'logout');
+        Route::post('/change-password', 'changePassword');
+    });
+});
 
 // USER ROUTES
 Route::controller(UserController::class)->group(function () {
@@ -15,7 +19,7 @@ Route::controller(UserController::class)->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('/user', 'profile');
         Route::get('/user/{id}', 'profile');
-        Route::patch('/user/update', 'update');
-        Route::delete('/user/delete', 'destroy');
+        Route::patch('/user', 'update');
+        Route::delete('/user', 'destroy');
     });
 });
